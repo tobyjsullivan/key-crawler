c70c0b297d697cf604ee2842ac37eb9d575b48b1
Toby Sullivan <tobyjsullivan@gmail.com>
2018-02-24 20:08:57 +1100

    Add skeleton for queuer

--- enum/key-enumerator.js
+++ enum/key-enumerator.js
@@ -5,7 +5,6 @@ var BigInteger = require('bigi');
 function enumerateKeys() {
 	var curPriv = BigInteger.ONE;
 
-	var i = 0;
 	while (true) {
 		var keyPair = new bitcoin.ECPair(curPriv);
 		curPriv = curPriv.add(BigInteger.ONE);
---
+++ queuer/app.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"github.com/urfave/negroni"
+)
+
+func main() {
+	keys := make(chan *keyPair, 20)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/pairs", pairsPostHandler(keys)).Methods(http.MethodPost)
+
+	n := negroni.New()
+	n.UseHandler(r)
+
+	port := "3000"
+
+	go queueSubmitter(keys)
+
+	n.Run(":"+port)
+}
+
+func pairsPostHandler(keys chan *keyPair) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		address := r.FormValue("address")
+		privateKey := r.FormValue("private-key")
+
+		if address == "" || privateKey == "" {
+			http.Error(w, "must provide address and private-key", http.StatusBadRequest)
+			return
+		}
+
+
+		keys <- &keyPair{
+			address: address,
+			privateKey: privateKey,
+		}
+	}
+}
+
+func queueSubmitter(keys chan *keyPair) {
+	for pair := range keys {
+		println("address:", pair.address, "privateKey:", pair.privateKey)
+
+		// TODO submit to a queue
+	}
+}
+
+type keyPair struct {
+	address string
+	privateKey string
+}
