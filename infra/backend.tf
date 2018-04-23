a8b215f74d3e0e00afaddb65a083dee6959e897b
Toby Sullivan <tobyjsullivan@gmail.com>
2018-04-23 21:56:45 +1000

    Move remote state

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform.tobysullivan.net"
-    key    = "key-crawler/terraform.tfstate"
+    bucket = "terraform-states.tobyjsullivan.com"
+    key    = "states/key-crawler/terraform.tfstate"
     region = "us-east-1"
   }
 }
